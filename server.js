dfe55ea499de8bd1bd66c74d4d8b9f1fd1267f63
aarongermaine <vcnts0@gmail.com>
2021-05-14 21:41:25 -0600

    basic file structure and server additions

---
+++ server.js
@@ -0,0 +1,31 @@
+//require an express top level for our server
+const express = require("express");
+
+//require mongo for our database
+const mongoose = require("mongoose");
+
+//establish our required port
+const PORT = process.env.PORT || 3000;
+
+//create the express application
+const app = express();
+
+// use our middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// connect to our mongo database
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+//api routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/views.js"));
+
+//listener callback
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
