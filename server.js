730b950860496dc5b2e1dc85c6f9df3572a315cc
aarongermaine <vcnts0@gmail.com>
2021-05-16 22:04:41 -0600

    deploy

--- server.js
+++ server.js
@@ -16,10 +16,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connect to our mongo database
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/myFirstDatabase",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 //api routes
 app.use(require("./routes/api.js"));
